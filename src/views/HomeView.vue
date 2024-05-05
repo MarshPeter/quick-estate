20d31fba6d5bba1d77cf050df4d865a98e96ab09
Peter Farmer <peter.m.farmer.204@gmail.com>
2024-05-05 23:35:37 +1000

    completed basic layout for mobile homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,29 @@
 <template>
     <div class="container">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 text-center">
+        <div class="container container-xl">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-12 col-lg-6 text-center">
                     <h1 class="display-1">QuickEstate</h1>
                 </div>
-                <div class="col-12 mb-4">
+                <div class="col-12 col-lg-6 mb-4">
                     <!-- created at: https://deepai.org/machine-learning-model/text2img -->
-                    <img class="hero-image" src="../assets/output.jpg" alt="">
+                    <img class="img-fluid img-thumbnail shadow-lg hero-image mx-auto d-block" src="../assets/output.jpg" alt="Dream house example">
                 </div>
                 <div class="col-12 gap-4">
-                    <div class="row gap-4 justify-content-center">
-                        <div class="col-9 bg-body-secondary p-4 border border-black rounded shadow text-center">
+                    <div class="row gap-4 justify-content-center align-content-center">
+                        <div class="col-9 col-md-3 bg-body-secondary p-4 border border-black rounded shadow text-center">
                             <p class="col m-0 fs-4">Are you looking for your next house?</p>
                         </div>
-                        <div class="col-9 bg-body-secondary p-4 border border-black rounded shadow text-center">
+                        <div class="col-9 col-md-3 bg-body-secondary p-4 border border-black rounded shadow text-center">
                             <p class="col m-0 fs-4">Perhaps you are looking to get the word out about an auction?</p>
                         </div>
-                        <div class="col-9 bg-body-secondary p-4 border border-black rounded shadow text-center">
+                        <div class="col-9 col-md-3 bg-body-secondary p-4 border border-black rounded shadow text-center">
                             <p class="col m-0 fs-4">Need a location to find and auction houses, but without a third-party?</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="container text-center mt-4 pt-2 pb-2 bg-body-secondary rounded shadow border border-black">
+            <div class="container container-xl text-center mt-4 pt-2 pb-2 bg-body-secondary rounded shadow border border-black">
                 <div class="row pb-2">
                     <h3>Trust Us Today!</h3>
                 </div>
@@ -35,7 +35,7 @@
                         <button type="button" class="btn btn-primary btn-lg">Find Next Home!</button>
                     </div>
                     <div class="col-8 d-grid">
-                        <button type="button" class="btn btn-outline btn-primary btn-lg">Register</button>
+                        <button type="button" class="btn btn-outline-primary btn-lg">Register</button>
                     </div>
                 </div>
             </div>
@@ -51,7 +51,6 @@
 
 <style scoped>
     .hero-image {
-        width: 100%;
         max-height: 500px;
     }
 </style>
